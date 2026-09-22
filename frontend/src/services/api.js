791bf3be1d61780dd156b92507863f34a6db1078
dikshitapoojari09 <dikshitapoojari09@gmail.com>
2026-09-22 18:09:36 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://project-task-management-platform-szyl.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
