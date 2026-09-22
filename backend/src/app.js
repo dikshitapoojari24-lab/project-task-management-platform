89c3c9b7e6f5715b13e3fceead2ce33d614aef45
dikshitapoojari09 <dikshitapoojari09@gmail.com>
2026-09-22 18:15:02 +0530

    Allow deployed frontend in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,10 +18,11 @@ app.use(helmet());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3000",
-    ],
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://localhost:3000",
+  "https://project-task-management-platform-nine.vercel.app",
+],
     credentials: true,
   })
 );
